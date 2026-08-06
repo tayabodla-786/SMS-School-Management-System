8ec211d8f77500dd50ee8534721581241754fdd0
Tayyab Imran <tayabodla787@example.com>
2026-08-06 17:36:39 +0500

    port

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   });
 
   const port = Number(process.env.PORT) || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`Application is running on: http://localhost:${port}`);
 }
