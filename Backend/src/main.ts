58c32034b52cc89a3212b672c9141e741244a8d7
Tayyab Imran <tayabodla787@example.com>
2026-08-07 13:15:33 +0500

    dockerfile

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost',
+      'https://sms-school-management-system-production-fb1f.up.railway.app/',
       'http://localhost:80',
       'http://localhost:5173',
       'http://127.0.0.1',
