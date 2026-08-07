fc24ba393832550f56b11d048fe810a9b65db98c
Tayyab Imran <tayabodla787@example.com>
2026-08-07 16:55:15 +0500

    main

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -5,21 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      
-      'https://sms-school-management-system-production-fb1f.up.railway.app',
-      
-    ],
+    origin: 'https://upbeat-joy-production-5029.up.railway.app',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
-
-  const port = Number(process.env.PORT) || 3000;
-  await app.listen(port, '0.0.0.0');
-
-  console.log(`Application is running on: http://localhost:${port}`);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 
 bootstrap();
