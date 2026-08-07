a9ffbfc27776856eef51444990e29ded42d6995f
Tayyab Imran <tayabodla787@example.com>
2026-08-07 16:51:29 +0500

    main.ts

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -6,12 +6,9 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'http://localhost:3000',
+      
       'https://sms-school-management-system-production-fb1f.up.railway.app',
-      'http://localhost:80',
-      'http://localhost:5173',
-      'http://127.0.0.1',
-      'http://127.0.0.1:80',
+      
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
